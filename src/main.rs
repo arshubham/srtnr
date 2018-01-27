ecdbffef4aadf4a2bbb87eeac8b894bf6afd32b0
Shubham Arora <shubhamarora@protonmail.com>
2018-01-27 15:41:49 +0530

    Add more Radio Button

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use gtk::prelude::*;
 use gio::prelude::*;
 use gtk::WidgetExt;
 use gtk::GridExt;
-use gtk::BoxExt;
 
 use urlshortener::{Provider, UrlShortener};
 
@@ -49,24 +48,33 @@ fn ui(app: &gtk::Application) {
     let shorten_url_button = gtk::Button::new_with_label("Shorten URL!");
 
     let short_url_label = gtk::Label::new ("");
+    shorten_url_button.set_margin_top (30);
 
+    let goo_gl_radiobutton = gtk::RadioButton::new_with_label("goo.gl");
+    let bit_ly_radiobutton = gtk::RadioButton::new_with_label_from_widget(&goo_gl_radiobutton, "bit.ly");
+    let is_gd_radiobutton = gtk::RadioButton::new_with_label_from_widget(&goo_gl_radiobutton, "is.gd");
+    let v_gd_radiobutton = gtk::RadioButton::new_with_label_from_widget(&goo_gl_radiobutton, "v.gd");
+    let tny_im_radiobutton = gtk::RadioButton::new_with_label_from_widget(&goo_gl_radiobutton, "tny.im");
+    let hmm_rs_radiobutton = gtk::RadioButton::new_with_label_from_widget(&goo_gl_radiobutton, "hmm.rs");
 
-    let is_gd_radiobutton = gtk::RadioButton::new_with_label("is.gd");
-    let v_gd_radiobutton = gtk::RadioButton::new_with_label_from_widget(&is_gd_radiobutton, "v.gd");
-    let radiobutton1clone = is_gd_radiobutton.clone ();
-    let radiobutton2clone = v_gd_radiobutton.clone ();
+    goo_gl_radiobutton.set_margin_top (30);
+    bit_ly_radiobutton.set_margin_top (30);
+    short_url_label.set_margin_top (20);
 
-    let radio_button_group = vec![radiobutton1clone, radiobutton2clone];
+    let goo_gl_rb_clone = goo_gl_radiobutton.clone ();
+    let bit_ly_rb_clone = bit_ly_radiobutton.clone ();
+    let is_gd_rb_clone = is_gd_radiobutton.clone ();
+    let v_gd_rb_clone = v_gd_radiobutton.clone ();
+    let tny_im_rb_clone = tny_im_radiobutton.clone ();
+    let hmm_rs_rb_clone = hmm_rs_radiobutton.clone ();
     let label_clone = short_url_label.clone ();
     let entry_clone = full_url_entry.clone ();
-
-    let radiobutton1clone1 = is_gd_radiobutton.clone ();
-    let radiobutton2clone2 = v_gd_radiobutton.clone ();
 
 	shorten_url_button.connect_clicked( move |_| {
 
 	    let url = gtk::EntryExt::get_text (&entry_clone).unwrap ();
 	    let us = UrlShortener::new().unwrap();
+
 	    let short_url = us.generate(url, &Provider::IsGd);
 
 	    let short_url = match short_url {
@@ -80,11 +88,17 @@ fn ui(app: &gtk::Application) {
 
 
     GridExt::attach (&main_grid, &url_label, 0, 0, 1, 1);
-	GridExt::attach (&main_grid, &full_url_entry , 1 , 0 ,4 ,1);
-	GridExt::attach (&main_grid, &shorten_url_button , 1 , 1 ,1 ,1);
-	GridExt::attach (&main_grid, &radiobutton1clone1, 1 ,2 ,1,1);
-	GridExt::attach (&main_grid, &radiobutton2clone2, 2, 2, 1, 1);
-	GridExt::attach (&main_grid, &short_url_label, 1, 4, 4, 1);
+	GridExt::attach (&main_grid, &full_url_entry , 1 , 0 ,3 ,1);
+
+    GridExt::attach (&main_grid, &goo_gl_rb_clone, 1 ,1 ,1,1);
+    GridExt::attach (&main_grid, &bit_ly_rb_clone, 2 ,1 ,1,1);
+	GridExt::attach (&main_grid, &is_gd_rb_clone, 0 ,2 ,1,1);
+	GridExt::attach (&main_grid, &v_gd_rb_clone, 1, 2, 1, 1);
+	GridExt::attach (&main_grid, &tny_im_rb_clone, 2 ,2 ,1,1);
+	GridExt::attach (&main_grid, &hmm_rs_rb_clone, 3 ,2 ,1,1);
+
+    GridExt::attach (&main_grid, &shorten_url_button , 1 , 3 ,2 ,1);
+	GridExt::attach (&main_grid, &short_url_label, 0, 4, 3, 1);
 
     window.add(&main_grid);
 
