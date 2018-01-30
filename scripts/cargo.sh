b184de15467dbd01c9190d1d7212f020eada289e
Shubham Arora <shubhamarora@protonmail.com>
2018-01-31 01:20:43 +0530

    Refactor meson

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-export CARGO_HOME=$1/target/cargo-home
+echo "RELEASE MODE"
+cargo build --release -p srtnr && cp $1/target/release/srtnr $2
 
-if [[ $DEBUG = true ]]
-then
-    echo "DEBUG MODE"
-    cargo build -p srtnr-gtk && cp $1/target/debug/srtnr-gtk $2
-else
-    echo "RELEASE MODE"
-    cargo build --release -p srtnr-gtk && cp $1/target/release/srtnr-gtk $2
-fi
