e3b9ab4c5c720b9ae199f003893a4ecbccd7e517
Shubham Arora <shubhamarora@protonmail.com>
2018-02-08 19:03:21 +0530

    Experiment with grid and action_widget

--- src/buildui/prefdialog/mod.rs
+++ src/buildui/prefdialog/mod.rs
@@ -1,34 +1,39 @@
 extern crate gtk;
 use gtk::IsA;
 
-use gtk::{Dialog, Grid, Label};
-use gtk::{DialogExt, GridExt, ContainerExt, WidgetExt, GtkWindowExt};
+use gtk::{Dialog, Label, Grid, Button};
+use gtk::{DialogExt, WidgetExt, GridExt, GtkWindowExt, ContainerExt, ButtonExt};
 
 pub struct PrefDialogUi {
-    pub dialog: Dialog,
+    pub pref_dialog: Dialog,
 }
 
 impl PrefDialogUi {
     pub fn new<P>(parent: &P) -> PrefDialogUi 
     where P: IsA<gtk::Window>, {
-
-    let dialog = Dialog::new ();
-    let grid = Grid::new ();
-    GridExt::set_row_spacing (&grid, 12);
-    GridExt::set_column_spacing (&grid, 6);
+    let pref_dialog = Dialog::new ();
+    let content_grid = Grid::new ();
     let label = Label::new_with_mnemonic (Some ("Some Text"));
-    GridExt::attach (&grid, &label, 0, 0, 1, 1);
-    DialogExt::get_content_area (&dialog).add (&grid);
-    dialog.set_transient_for (parent);
+    GridExt::attach (&content_grid, &label, 0, 0, 1, 1);
+    DialogExt::get_content_area (&pref_dialog).add (&content_grid);
+    GtkWindowExt::set_transient_for (&pref_dialog, parent);
+    
+    let close_button = Button::new_with_label("Close");
+    DialogExt::add_action_widget (&pref_dialog, &close_button, 0);
+    DialogExt::get_content_area (&pref_dialog).show_all ();
+
+    let pref_dialog_clone = pref_dialog.clone ();
+    close_button.connect_clicked (move |_| {
+        pref_dialog_clone.emit_close ();
+    });
 
     PrefDialogUi {
-             dialog,
+             pref_dialog,
         }
     }
 
     pub fn run(&self) { 
-
-        WidgetExt::show_now (&self.dialog);
+        WidgetExt::show_now (&self.pref_dialog);
     }
 
 }
